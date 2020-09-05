df14c099239147cd896afa15c11623dcc07c199c
张俊 <873316162@qq.com>
2020-09-05 22:52:12 +0800

    获取用户信息

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,4 +5,29 @@ $(function(){
         // token不存在，跳转到登陆页面
         location.href='./login.html'
     }
+    // 首页加载时需要调用后台接口获取用户信息
+    function loadUserInfo(){
+        $.ajax({
+            type:'get',
+            url:'http://ajax.frontend.itheima.net/my/userinfo',
+            headers:{
+                Authorization:localStorage.getItem('mytoken')
+            },
+            success:function(res){
+                //获取用户信息
+                // 先判断状态
+                if(res.status===0){
+                    var info=res.data
+                    // 把用户信息填充到指定位置
+                    $('#welcome-username').html(info.username)
+                    $('#nav-username').html(info.username)
+                    // 填充头像信息
+                    if(info.user_pic){
+                        // 存在头像数据
+                    }
+                }
+            }
+        })
+    }
+    loadUserInfo()
 })
