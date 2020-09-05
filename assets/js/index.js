cd52ac572c6520b7dbaa4f15c963f7d7f0f99418
张俊 <873316162@qq.com>
2020-09-05 23:02:57 +0800

    头像信息处理

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,8 +22,16 @@ $(function(){
                     $('#welcome-username').html(info.username)
                     $('#nav-username').html(info.username)
                     // 填充头像信息
+                    // info.user_pic="http://t.cn/RCzsdCq"
                     if(info.user_pic){
-                        // 存在头像数据
+                        // 存在头像数据,显示一张图片
+                        // 删除默认头像
+                        $('#welcome-username').parent().prev('div').remove()
+                        // 添加新的头像
+                        $('#welcome-username').parent()
+                        .prepend('<img src="'+info.user_pic+'"alt=""/>')
+                    }else{
+                        // 不存在显示div
                     }
                 }
             }
