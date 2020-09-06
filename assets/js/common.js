9fd3d18cde9f1aa02953c14def4a719e74c18ba6
张俊 <873316162@qq.com>
2020-09-06 22:17:20 +0800

    实现请求进度条控制

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,26 +1,40 @@
-// 通用的接口调用设置
-var baseURL='http://ajax.frontend.itheima.net/'
-$.ajaxPrefilter(function(option){
-    // 行参option是jQuery请求方法的配置信息
-    // 1.配置通用的url地址
-    option.url=baseURL+option.url
-    // 2.设置接口的请求头信息
-    if(option.url.lastIndexOf('/my')!==-1){
-        // 所以包含my的请求路径需要进行权限验证
-        // headers默认不存在，所以需要设置一个对象
-        option.headers={
-            Authorization:localStorage.getItem('mytoken')
-        }
+/*
+  通用的接口调用设置
+*/
+var baseURL = 'http://ajax.frontend.itheima.net/'
+
+$.ajaxPrefilter(function (option) {
+  // 形参option是jQuery请求方法的配置信息
+  // 发送请求之前会触发beforeSend
+  option.beforeSend = function () {
+    // 发送请求之前开始进度条
+    NProgress && NProgress.start()
+    // if (NProgress) {
+    //   NProgress.start()
+    // }
+  }
+
+  // 1、配置通用的URL地址
+  option.url = baseURL + option.url
+  // 2、设置接口的通用请求头信息
+  if (option.url.lastIndexOf('/my/') !== -1) {
+    // 所有包含my的请求路径需要进行权限验证（需要先登录）
+    // header默认不存在，所以需要设置一个对象
+    option.headers = {
+      Authorization: localStorage.getItem('mytoken')
     }
-    // 3.设置通用的异常情况
-    // 服务器响应结束时触发
-    option.complete=function(res){
-        // 处理失败的情况
-        if(res.responseJSON.status===1&&res.responseJSON.message==='身份认证失败！'){
-            // 删除无效token
-            localStorage.removeItem('mytoken')
-            // 身份验证失败，跳转到登陆页面
-            location.href='./login.html'
-        }
+  }
+  // 3、处理通用的异常情况
+  // 服务器响应结束时触发
+  option.complete = function (res) {
+    // 完成请求后，结束进度条
+    NProgress && NProgress.done()
+    // 处理失败的情况
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 把无效的token清除
+      localStorage.removeItem('mytoken')
+      // 如果身份验证失败了，就跳转到登录页面
+      location.href = './login.html'
     }
+  }
 })
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,54 +1,58 @@
 $(function(){
     // 判断token是否存在
-    var mytoken=localStorage.getItem('mytoken')
-    if(!mytoken){
-        // token不存在，跳转到登陆页面
-        location.href='./login.html'
+    var mytoken = localStorage.getItem('mytoken')
+    if (!mytoken) {
+        // 表示token不存在，跳转到登录页面
+        location.href = './login.html'
     }
     // 首页加载时需要调用后台接口获取用户信息
-    function loadUserInfo(){
+    function loadUserInfo () {
         $.ajax({
-            type:'get',
-            url:'my/userinfo',
-            // headers:{
-            //     //凡是以my开头的请求都需要携带请求头，作用：权限验证（登录后才能访问）
-            //     Authorization:localStorage.getItem('mytoken')
-            // },
-            success:function(res){
-                //获取用户信息
-                // 先判断状态
-                if(res.status===0){
-                    var info=res.data
-                    // 把用户信息填充到指定位置
-                    $('#welcome-username').html(info.username)
-                    $('#nav-username').html(info.username)
-                    // 填充头像信息
-                    // info.user_pic="http://t.cn/RCzsdCq"
-                    if(info.user_pic){
-                        // 存在头像数据,显示一张图片
-                        // 删除默认头像
-                        $('#welcome-username').parent().prev('div').remove()
-                        // 添加新的头像
-                        $('#welcome-username').parent()
-                        .prepend('<img src="'+info.user_pic+'"alt=""/>')
-                    }else{
-                        // 不存在显示div
-                    }
-                }
+        type: 'get',
+        url: 'http://ajax.frontend.itheima.net/my/userinfo',
+        headers: {
+            Authorization: localStorage.getItem('mytoken')
+        },
+        success: function (res) {
+            if (res.status === 0 ) {
+            // 获取用户信息
+            var info = res.data
+            // 把用户信息填充到指定的位置
+            // 填充用户名
+            $('#welcome-username').html(info.username)
+            $('#nav-username').html(info.username)
+            // 填充头像信息
+            // info.user_pic = 'http://t.cn/RCzsdCq'
+            if (info.user_pic) {
+                // 存在头像数据,显示一张图片
+                // 删除默认的头像
+                $('#welcome-username')
+                .parent()
+                .prev('div')
+                .remove()
+                // 添加新的头像
+                $('#welcome-username')
+                .parent()
+                .prepend('<img src="'+info.user_pic+'" alt="" />')
+            } else {
+                // 头像不存在，显示Div
+    
             }
+            }
+        }
         })
     }
     loadUserInfo()
 
     // 绑定退出按钮的点击事件
-    $('#logout-btn').click(function(){
-        layer.confirm('is not?', {icon: 3, title:'提示'}, function(index){
-        //实现退出功能:跳转到登陆页面，删除数据
+    $('#logout-btn').click(function () {
+        layer.confirm('确认要退出吗?', {icon: 3, title:'提示'}, function(index){
+          // 实现退出的功能：清除token,跳转到登录页面
             localStorage.removeItem('mytoken')
-        // 关闭弹窗
-        layer.close(index)
-        // 跳转到登陆页面
-        location.href='./login.html'
+          // 关闭弹窗
+            layer.close(index)
+          // 跳转到登录页面
+            location.href = './login.html'
         });
     })
     
