ae78f1cbcc375d0a4ed8207a6e3584d2524aeea8
张俊 <873316162@qq.com>
2020-09-07 21:47:44 +0800

    修改密码表单验证

--- assets/js/my/my-repwd.js
+++ assets/js/my/my-repwd.js
@@ -1,4 +1,26 @@
 $(function(){
+    // 处理表单验证
+    var form=layui.form
+    // 自定义验证规则
+    // 1.新密码和原来密码不一样
+    // 2.确认密码必须一致
+    form.verify({
+        diff:function(value){
+            // 密码和原来密码不一样
+            // 获取原来密码
+            var oldPwd=$('#form input[name=oldPwd]').val()
+            if(oldPwd===value){
+                return '新密码不能与原密码相同'
+            }
+        },
+        same:function(){
+             // 获取原来密码
+             var oldPwd=$('#form input[name=newPwd]').val()
+             if(newPwd!==value){
+                 return '两次输入的密码不相同'
+             }
+        }
+    })
     // 控制表单提交
     $('#form').submit(function(e){
         e.preventDefault()
