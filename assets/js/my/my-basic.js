01bc2c14fbee8bd9461e8feded0700d7f82e4e28
张俊 <873316162@qq.com>
2020-09-07 20:27:13 +0800

    基于layui方法填充表单

--- assets/js/my/my-basic.js
+++ assets/js/my/my-basic.js
@@ -1,4 +1,6 @@
 $(function(){
+    // 获取layui的form对象
+    var form=layui.form
     // 调用接口加载用户信息
     function loadUserInfo(){
         $.ajax({
@@ -7,9 +9,11 @@ $(function(){
             success:function(){
                 // 把数据填充到表单
                 // $('#form input[name=id]').val(res.data.id)
-                $('#form input[name=username]').val(res.data.username)
-                $('#form input[name=nickname]').val(res.data.nickname)
-                $('#form input[name=email]').val(res.data.email)
+                // $('#form input[name=username]').val(res.data.username)
+                // $('#form input[name=nickname]').val(res.data.nickname)
+                // $('#form input[name=email]').val(res.data.email)
+                // 基于layui填充表单
+                form.val('basicForm',res.data)
             }
         })
     }
